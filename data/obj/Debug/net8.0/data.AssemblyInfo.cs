42fc233534642785efa5e6a3f2f99da6bcfde8ef
Lorenzo Gugliara <guguxd@hotmail.it>
2024-02-06 00:47:37 +0100

    Update data and api projects

--- data/obj/Debug/net8.0/data.AssemblyInfo.cs
+++ data/obj/Debug/net8.0/data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1fd90b2207f572b1ad2755c419749e9a1c631f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d089ffea83f10f52ea359d72b7f2e38dce82ec3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
